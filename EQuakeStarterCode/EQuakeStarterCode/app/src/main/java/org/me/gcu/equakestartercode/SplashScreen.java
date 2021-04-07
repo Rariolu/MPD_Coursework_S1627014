484a058b10bf25d0e0ad3a0207a209be1c984ff7
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-04-07 18:24:34 +0100

    Added splash screen

---
+++ EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SplashScreen.java
@@ -0,0 +1,38 @@
+package org.me.gcu.equakestartercode;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class SplashScreen extends AppCompatActivity
+{
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnStart.setOnClickListener((View view) ->
+        {
+            SwitchActivities();
+        });
+
+    }
+
+    private void SwitchActivities()
+    {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
+    }
+}
