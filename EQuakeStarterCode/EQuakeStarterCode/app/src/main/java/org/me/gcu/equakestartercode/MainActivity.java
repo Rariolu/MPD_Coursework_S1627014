8450221d70b8a04e5c92e8c6094ff5c93656b01f
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-04-07 18:28:11 +0100

    Implemented ListView

--- EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/MainActivity.java
+++ EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/MainActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,12 +20,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener
 {
-    private TextView rawDataDisplay;
+    //private TextView rawDataDisplay;
     private Button startButton;
+    private ListView lstItems;
     private String result="";
     private String url1="";
     private String urlSource="http://quakes.bgs.ac.uk/feeds/MhSeismology.xml";
@@ -33,8 +40,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
         setContentView(R.layout.activity_main);
         Log.e("MyTag","in onCreate");
         // Set up the raw links to the graphical components
-        rawDataDisplay = (TextView)findViewById(R.id.rawDataDisplay);
+        //rawDataDisplay = (TextView)findViewById(R.id.rawDataDisplay);
         startButton = (Button)findViewById(R.id.startButton);
+        lstItems = (ListView)findViewById(R.id.lstItems);
+
         startButton.setOnClickListener(this);
         Log.e("MyTag","after startButton");
         // More Code goes here
@@ -51,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
     {
         // Run network access on a separate thread;
         new Thread(new Task(urlSource)).start();
-    } //
+    }
 
     // Need separate thread to access the internet resource over network
     // Other neater solutions should be adopted in later iterations.
@@ -63,10 +72,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
         {
             url = aurl;
         }
+
         @Override
         public void run()
         {
-
             URL aurl;
             URLConnection yc;
             BufferedReader in = null;
@@ -82,16 +91,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
                 yc = aurl.openConnection();
                 in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
                 Log.e("MyTag","after ready");
-                //
-                // Throw away the first 2 header lines before parsing
-                //
-                //
-                //
+
                 while ((inputLine = in.readLine()) != null)
                 {
                     result = result + inputLine;
-                    //Log.e("MyTag",inputLine);
-
                 }
                 in.close();
             }
@@ -104,7 +107,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
 
             MainActivity.this.runOnUiThread(new Runnable()
             {
-                public void run() {
+                public void run()
+                {
                     Log.d("UI thread", "I am the UI thread");
 
                     LinkedList<Item> items = channel.GetItems();
@@ -113,18 +117,33 @@ public class MainActivity extends AppCompatActivity implements OnClickListener
 
                     Log.e("debug","gets to ui thread");
 
-                    for (int i = 0; i <items.size(); i++)
+                    ArrayList<String> itemStr = new ArrayList<>();
+
+                    for (Item item : items)
+                    {
+                        itemStr.add(item.toString());
+                    }
+
+                    Log.e("debug", String.valueOf(itemStr.size()));
+
+                    ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.activity_listview, itemStr);
+
+                    lstItems.setAdapter(adapter);
+
+                    Utility.setListViewHeightBasedOnChildren(lstItems);
+
+                    /*for (int i = 0; i <items.size(); i++)
                     {
                         Item item = items.get(i);
                         //output += "Item Category: "+item.GetCategory();
                         output += "Item Description: "+item.GetDescription();
-                    }
+                    }*/
 
-                    rawDataDisplay.setText(output);
+                    //rawDataDisplay.setText(output);
                 }
             });
         }
 
     }
-
 }
+
