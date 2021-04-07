cb72275349ea130ba6dd4d54f55dea754464edbf
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-04-07 18:14:53 +0100

    Added Utility class to set list view size

---
+++ EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/Utility.java
@@ -0,0 +1,43 @@
+//Robbie Coyne S1627014
+
+package org.me.gcu.equakestartercode;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+//Salvaged from
+//https://stackoverflow.com/questions/3495890/how-can-i-put-a-listview-into-a-scrollview-without-it-collapsing/3495908#3495908
+//as a solution to an issue involving the height not adapting properly
+public class Utility
+{
+    public static void setListViewHeightBasedOnChildren(ListView listView)
+    {
+        ListAdapter listAdapter = listView.getAdapter();
+
+        if (listAdapter == null)
+        {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = listView.getPaddingTop() + listView.getPaddingBottom();
+
+        for (int i = 0; i < listAdapter.getCount(); i++)
+        {
+            View listItem = listAdapter.getView(i, null, listView);
+            if (listItem instanceof ViewGroup)
+            {
+                listItem.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }
+}
