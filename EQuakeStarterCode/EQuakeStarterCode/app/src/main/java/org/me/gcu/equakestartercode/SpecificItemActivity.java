0bb2804fd5a2740521c486a06e97c90d3e17353e
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-04-09 06:15:04 +0100

    Implemented map into specific item activity

--- EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SpecificItemActivity.java
+++ EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SpecificItemActivity.java
@@ -2,6 +2,11 @@ package org.me.gcu.equakestartercode;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,11 +20,13 @@ import org.w3c.dom.Text;
 
 public class SpecificItemActivity extends AppCompatActivity
 {
+    private GoogleMap mMap;
     TextView lblTitle;
     TextView lblCoordinates;
     TextView lblMagnitude;
     TextView lblDepth;
     TextView lblDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -39,5 +46,15 @@ public class SpecificItemActivity extends AppCompatActivity
         lblMagnitude.setText("Magnitude: "+String.valueOf(thisItem.GetMagnitude()));
         lblDepth.setText("Depth: "+String.valueOf(thisItem.GetDepth())+"km");
         lblDate.setText("Date: "+String.valueOf(thisItem.GetPublicationDate()));
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync((GoogleMap googleMap) ->
+        {
+            mMap = googleMap;
+
+            LatLng itemPos = new LatLng(thisItem.GetLatitude(),thisItem.GetLongitude());
+            mMap.addMarker(new MarkerOptions().position(itemPos).title(thisItem.GetLocation()));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(itemPos));
+        });
     }
 }
