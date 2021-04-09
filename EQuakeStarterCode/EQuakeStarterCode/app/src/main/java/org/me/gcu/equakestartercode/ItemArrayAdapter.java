5efa6d14b3d9493804e5e696a36f41fa200041ac
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-04-09 02:44:51 +0100

    Implemented sorting

--- EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/ItemArrayAdapter.java
+++ EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/ItemArrayAdapter.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,18 +16,138 @@ import android.widget.TextView;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Function;
 
 public class ItemArrayAdapter extends ArrayAdapter<Item>
 {
     private final Context thisContext;
     private final ArrayList<Item> items;
-    public ItemArrayAdapter(@NonNull Context context, ArrayList<Item> list)
+    private final SearchParams thisSearchParams;
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public ItemArrayAdapter(@NonNull Context context, ArrayList<Item> list, SearchParams searchParams)
     {
         super(context, 0, list);
         thisContext = context;
+        Function<Item, Boolean> itemInRange = (Item item) ->
+        {
+            DateTime itemDate = item.GetDate();
+            if (searchParams.UsingDataRange())
+            {
+                DateTime startDate = searchParams.GetStartDate();
+                DateTime endDate = searchParams.GetEndDate();
+                boolean minConstraint = itemDate.IsAfter(startDate) || itemDate.equals(startDate);
+                boolean maxConstraint = !itemDate.IsAfter(endDate) || itemDate.equals(endDate);
+                return minConstraint && maxConstraint;
+            }
+            return true;
+        };
+        if (searchParams.Sort())
+        {
+            Comparator<Item> itemSort = (Item item1, Item item2) ->
+            {
+                Boolean item1InRange = itemInRange.apply(item1);
+                Boolean item2InRange = itemInRange.apply(item2);
+                if (item1InRange && item2InRange)
+                {
+                    switch (searchParams.GetSortType())
+                    {
+                        case STRONGEST_TO_WEAKEST:
+                        {
+                            return item1.GetMagnitude() > item2.GetMagnitude() ? -1 : 1;
+                        }
+                        case WEAKEST_TO_STRONGEST:
+                        {
+                            return item1.GetMagnitude() < item2.GetMagnitude() ? -1 : 1;
+                        }
+                        case MOST_RECENT_TO_LEAST_RECENT:
+                        {
+                            Log.e("debug", "mostrecenttoleast");
+                            return item1.GetDate().IsAfter(item2.GetDate()) ? -1 : 1;
+                        }
+                        case LEAST_RECENT_TO_MOST_RECENT:
+                        {
+                            return item2.GetDate().IsAfter(item1.GetDate()) ? -1 : 1;
+                        }
+                        case NORTHMOST_TO_SOUTHMOST:
+                        {
+                            return item1.GetLatitude() > item2.GetLatitude() ? -1 : 1;
+                        }
+                        case SOUTHMOST_TO_NORTHMOST:
+                        {
+                            return item1.GetLatitude() < item2.GetLatitude() ? -1 : 1;
+                        }
+                        case EASTMOST_TO_WESTMOST:
+                        {
+                            return item1.GetLongitude() < item2.GetLongitude() ? -1 : 1;
+                        }
+                        case WESTMOST_TO_EASTMOST:
+                        {
+                            return item1.GetLongitude() > item2.GetLongitude() ? -1 : 1;
+                        }
+                        case DEEPEST_TO_SHALLOWEST:
+                        {
+                            return item1.GetDepth() > item2.GetDepth() ? -1 : 1;
+                        }
+                        case SHALLOWEST_TO_DEEPEST:
+                        {
+                            return item1.GetDepth() < item2.GetDepth() ? -1 : 1;
+                        }
+                        default:
+                        {
+                            Log.e("debug", "default ussed");
+                            return 0;
+                        }
+                    }
+                }
+                else if (item1InRange)
+                {
+                    return -1;
+                }
+                return 1;
+            };
+
+            list.sort(itemSort);
+            Log.e("debug","Sorting attempted");
+        }
+        else if (searchParams.UsingSpecificDate())
+        {
+            Comparator<Item> itemSort = (Item item1, Item item2) ->
+            {
+                DateTime specificDate = searchParams.GetSpecificDate();
+                boolean equal = item1.GetDate().equals(item2.GetDate());
+                if (equal)
+                {
+                    return 0;
+                }
+                return item1.GetDate().equals(specificDate) ? -1 : (item2.GetDate().equals(specificDate) ? 1 : 0);
+            };
+            list.sort(itemSort);
+        }
+        else if (searchParams.UsingDataRange())
+        {
+            Comparator<Item> itemSort = (Item item1, Item item2) ->
+            {
+                Boolean item1InRange = itemInRange.apply(item1);
+                Boolean item2InRange = itemInRange.apply(item2);
+                if (item1InRange && item2InRange)
+                {
+                    return 0;
+                }
+                else if (item1InRange)
+                {
+                    return -1;
+                }
+                return 1;
+            };
+            list.sort(itemSort);
+        }
+
         items = list;
+        thisSearchParams = searchParams;
     }
 
     @SuppressLint("ResourceAsColor")
@@ -67,8 +188,6 @@ public class ItemArrayAdapter extends ArrayAdapter<Item>
             //colorNum = R.color.DarkRed;
         }
 
-        Log.e("debug","magNorm: "+String.valueOf(magNorm)+"; colorNum: "+String.valueOf(colorNum));
-
         textView.setBackgroundColor(colorNum);
 
         //textView.setBackgroundColor();
@@ -80,6 +199,26 @@ public class ItemArrayAdapter extends ArrayAdapter<Item>
             thisContext.startActivity(intent);
         });
 
+        DateTime itemDate = item.GetDate();
+        if (thisSearchParams.UsingSpecificDate())
+        {
+            DateTime date = thisSearchParams.GetSpecificDate();
+            if (!itemDate.equals(date))
+            {
+                textView.setVisibility(View.GONE);
+            }
+        }
+        else if (thisSearchParams.UsingDataRange())
+        {
+            DateTime startDate = thisSearchParams.GetStartDate();
+            DateTime endDate = thisSearchParams.GetEndDate();
+            boolean minConstraint = itemDate.IsAfter(startDate) || itemDate.equals(startDate);
+            boolean maxConstraint = !itemDate.IsAfter(endDate) || itemDate.equals(endDate);
+            if (!(minConstraint && maxConstraint))
+            {
+                textView.setVisibility(View.GONE);
+            }
+        }
         return textView;
     }
 }
