5b369de5d333047a37ebd60a126ae008eb0389c8
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-04-09 02:44:31 +0100

    Added coordinates, depth, and date.

--- EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SpecificItemActivity.java
+++ EQuakeStarterCode/EQuakeStarterCode/app/src/main/java/org/me/gcu/equakestartercode/SpecificItemActivity.java
@@ -11,10 +11,15 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class SpecificItemActivity extends AppCompatActivity
 {
     TextView lblTitle;
+    TextView lblCoordinates;
     TextView lblMagnitude;
+    TextView lblDepth;
+    TextView lblDate;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -22,12 +27,17 @@ public class SpecificItemActivity extends AppCompatActivity
         setContentView(R.layout.activity_specific_item);
 
         lblTitle = (TextView)findViewById(R.id.lblTitle);
+        lblCoordinates = (TextView)findViewById(R.id.lblCoordinates);
         lblMagnitude = (TextView)findViewById(R.id.lblMagnitude);
-
+        lblDepth = (TextView)findViewById(R.id.lblDepth);
+        lblDate = (TextView)findViewById(R.id.lblDate);
 
         Item thisItem = (Item)getIntent().getSerializableExtra("item");
-        lblTitle.setText(thisItem.GetLocation());
 
+        lblTitle.setText(thisItem.GetLocation());
+        lblCoordinates.setText("Latitude: "+String.valueOf(thisItem.GetLatitude())+"; Longitude: "+String.valueOf(thisItem.GetLongitude()));
         lblMagnitude.setText("Magnitude: "+String.valueOf(thisItem.GetMagnitude()));
+        lblDepth.setText("Depth: "+String.valueOf(thisItem.GetDepth()));
+        lblDate.setText("Date: "+String.valueOf(thisItem.GetPublicationDate()));
     }
 }
